c3e93cc4f117d5d2549450c497eb421472db4b9c
SamuelVn007 <134400857+SamuelVn007@users.noreply.github.com>
2024-09-05 12:09:24 -0300

    Add files via upload

---
+++ cliente.py
@@ -0,0 +1,12 @@
+class Clientes:
+    nome = ''
+    instrumento_musical = ''
+    idade = ''
+    cadastro = False
+
+    cliente_Marcos = Clientes()
+    cliente_Rafaela = Clientes()
+
+    Clientes =[cliente_Marcos, cliente_Rafaela]
+
+    
